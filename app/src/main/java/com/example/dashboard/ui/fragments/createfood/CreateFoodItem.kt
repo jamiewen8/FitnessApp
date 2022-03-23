16a4ee2a81d08177af016bdf28861bce02d6f9d2
ThomasHensby <thomashensby@gmail.com>
2022-03-23 18:35:18 +0000

    Got it toi search db when click on image

--- app/src/main/java/com/example/dashboard/ui/fragments/createfood/CreateFoodItem.kt
+++ app/src/main/java/com/example/dashboard/ui/fragments/createfood/CreateFoodItem.kt
@@ -1,6 +1,7 @@
 package com.example.dashboard.ui.fragments.createfood
 
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -48,7 +49,6 @@ class CreateFoodItem : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        observerSetup()
         _binding = FragmentCreateFoodItemBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -66,18 +66,47 @@ class CreateFoodItem : Fragment() {
     private fun listenerSetup(){
 
         binding.something1.setOnClickListener{
-            var drawableSelected = 1
-            viewModel.findFoodByImage(drawableSelected)
+            viewModel.findFood(1)
             binding.something2.alpha = 0.5f
             binding.something3.alpha = 0.5f
             binding.something4.alpha = 0.5f
             binding.something5.alpha = 0.5f
         }
+        binding.something2.setOnClickListener{
+            viewModel.findFood(2)
+            binding.something1.alpha = 0.5f
+            binding.something3.alpha = 0.5f
+            binding.something4.alpha = 0.5f
+            binding.something5.alpha = 0.5f
+        }
+        binding.something3.setOnClickListener{
+            viewModel.findFood(3)
+            binding.something1.alpha = 0.5f
+            binding.something2.alpha = 0.5f
+            binding.something4.alpha = 0.5f
+            binding.something5.alpha = 0.5f
+        }
+        binding.something4.setOnClickListener{
+            viewModel.findFood(4)
+            binding.something1.alpha = 0.5f
+            binding.something2.alpha = 0.5f
+            binding.something3.alpha = 0.5f
+            binding.something5.alpha = 0.5f
+        }
+        binding.something5.setOnClickListener{
+            viewModel.findFood(5)
+            binding.something2.alpha = 0.5f
+            binding.something3.alpha = 0.5f
+            binding.something4.alpha = 0.5f
+            binding.something1.alpha = 0.5f
+        }
+
     }
 
 
+    @SuppressLint("FragmentLiveDataObserve")
     private fun observerSetup() {
-        viewModel.getSearchResultsImage().observe(viewLifecycleOwner) { foods ->
+        viewModel.getSearchResults().observe(this) { foods ->
             foods?.let {
                 if (it.isNotEmpty()) {
                     binding.protein.text = it[0].protein
